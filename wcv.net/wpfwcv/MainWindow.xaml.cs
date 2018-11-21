e65def104c44d1784dd8199606e7f60a4228fb12
Горин Александр <pu1s@outlook.com>
2018-11-21 22:30:51 +0400

    ////////////////////////////////////////

--- wcv.net/wpfwcv/MainWindow.xaml.cs
+++ wcv.net/wpfwcv/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using libwcvnet;
 
 namespace wpfwcv
 {
@@ -23,6 +24,11 @@ namespace wpfwcv
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClipboardViewerForm f = new ClipboardViewerForm();
         }
     }
 }
